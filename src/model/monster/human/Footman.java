f341012e445e3e7316ed2df8daf3eb692b0c49a9
avaricak <avaricak@2bbab176-0796-11df-956f-01f849d29158>
2010-01-22 22:12:14 +0000

    Napravljene osnove

---
+++ src/model/monster/human/Footman.java
@@ -0,0 +1,21 @@
+package model.monster.human;
+
+
+public class Footman extends Human {
+
+	public Footman(){
+		setStrength(65);
+		setInteligence(38);
+		setAgility(51);
+		setVitality(100);
+		setMinDamage(11);
+		setMaxDamage(12);
+		setActionPoints(6);
+		setDefence(12);
+		setRange(1);
+		setWaterResistance(-15);
+		setFireResistance(-5);
+		setEarthResistance(30);
+		setAirResistance(25);
+	}
+}
