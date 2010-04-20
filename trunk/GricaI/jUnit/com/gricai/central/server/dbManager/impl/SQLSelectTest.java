abbedc36d77cbea07d60a3ecdb5487f95a6113e4
avaricak <avaricak@2bbab176-0796-11df-956f-01f849d29158>
2010-04-20 17:02:53 +0000

    dodati neki primeri radi dalje implementacije

--- trunk/GricaI/jUnit/com/gricai/central/server/dbManager/impl/SQLSelectTest.java
+++ trunk/GricaI/jUnit/com/gricai/central/server/dbManager/impl/SQLSelectTest.java
@@ -8,7 +8,7 @@ import org.junit.*;
 
 public class SQLSelectTest {
 
-	SQLSelect select = new SQLSelectImpl();
+	SQLSelect select;
 
 	@Before
 	// Will be performed before each test.
@@ -27,13 +27,14 @@ public class SQLSelectTest {
 	@Test
 	// tests SQLSelectImpl
 	public void test1()	{
-		select = new SQLSelectImpl();
+		
 		try
 		{
+			select = SQLSelect.getInstance();
 			assertEquals("SELECT nesto FROM negde", select.select("nesto").from("negde").evaluate());
-			assertEquals("SELECT nesto FROM negde,negde2 WHERE nesto", new SQLSelectImpl().select("nesto").from("negde").from("negde2").where("nesto").evaluate());
+			assertEquals("SELECT nesto FROM negde,negde2 WHERE nesto", SQLSelect.getInstance().select("nesto").from("negde").from("negde2").where("nesto").evaluate());
 			System.out.println("Test 1 completed successfully.");
-		} catch (NullPointerException e){
+		} catch (Exception e){
 			System.out.println(e.getMessage());
 		}
 	}
@@ -41,14 +42,15 @@ public class SQLSelectTest {
 	@Test
 	//Tests SQLSelectImpl exceptions
 	public void test2(){
-		select =  new SQLSelectImpl();
+		
 		try{
-			String s = select.evaluate();
-		} catch (NullPointerException e){
+			select =  SQLSelect.getInstance();
+			select.evaluate();
+		} catch (Exception e){
 			assertEquals("Null pointer exception:empty SELECT string", e.getMessage());
 		}
 		try{
-			String s = select.select("nesto").evaluate();
+			select.select("nesto").evaluate();
 		} catch ( NullPointerException e){
 			assertEquals("Null pointer exception:empty FROM string", e.getMessage());
 		}
--- trunk/GricaI/src/com/gricai/central/server/dbManager/SQLSelect.java
+++ trunk/GricaI/src/com/gricai/central/server/dbManager/SQLSelect.java
@@ -1,47 +1,27 @@
 package com.gricai.central.server.dbManager;
 
+import java.lang.reflect.Constructor;
+
 import com.gricai.central.server.dbManager.impl.SQLSelectImpl;
 
 public abstract class SQLSelect extends SQLQuery{
-	
-	
-	//strings for saving select procedure arguments
-	private String selectString = null;
-	private String fromString = null;
-	private String whereString = null;
-	private String orderByString = null;
-	
-	public String getOrderByString() {
-		return orderByString;
-	}
-	public void setOrderByString(String orderByString) {
-		this.orderByString = orderByString;
-	}
-	public String getSelectString() {
-		return selectString;
-	}
-	public void setSelectString(String selectString) {
-		this.selectString = selectString;
-	}
-	public String getFromString() {
-		return fromString;
-	}
-	public void setFromString(String fromString) {
-		this.fromString = fromString;
-	}
-	public String getWhereString() {
-		return whereString;
-	}
-	public void setWhereString(String whereString) {
-		this.whereString = whereString;
-	}
 	
 	public abstract SQLSelect select(String columnName);
 	public abstract SQLSelect from(String tablename);
 	public abstract SQLSelect where(String condition);
 	public abstract SQLSelect orderBy(String condition);
+	public abstract SQLSelect parameter(String paramName, Object paramValue);
+	public abstract SQLSelect or();
+	public abstract SQLSelect and();
 	
-	public static SQLSelect getInstance(){
-		return new SQLSelectImpl();
+	public static SQLSelect getInstance() throws Exception {
+		try{
+			Constructor<SQLSelectImpl> constructor = SQLSelectImpl.class.getDeclaredConstructor();
+			constructor.setAccessible(true);
+			return constructor.newInstance();
+		}catch (Exception e) {
+			//TODO log
+			throw e;
+		}
 	}
 }
