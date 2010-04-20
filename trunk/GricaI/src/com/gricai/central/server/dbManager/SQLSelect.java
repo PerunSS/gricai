d867ada42fb7e88233d67ab7dc21816d16704591
churava13 <churava13@2bbab176-0796-11df-956f-01f849d29158>
2010-04-20 12:28:16 +0000

    SQLSelectImpl, SQLSelect implemented

--- trunk/GricaI/src/com/gricai/central/server/dbManager/SQLSelect.java
+++ trunk/GricaI/src/com/gricai/central/server/dbManager/SQLSelect.java
@@ -3,7 +3,39 @@ package com.gricai.central.server.dbManager;
 import com.gricai.central.server.dbManager.impl.SQLSelectImpl;
 
 public abstract class SQLSelect extends SQLQuery{
-
+	
+	
+	//strings for saving select procedure arguments
+	private String selectString = null;
+	private String fromString = null;
+	private String whereString = null;
+	private String orderByString = null;
+	
+	public String getOrderByString() {
+		return orderByString;
+	}
+	public void setOrderByString(String orderByString) {
+		this.orderByString = orderByString;
+	}
+	public String getSelectString() {
+		return selectString;
+	}
+	public void setSelectString(String selectString) {
+		this.selectString = selectString;
+	}
+	public String getFromString() {
+		return fromString;
+	}
+	public void setFromString(String fromString) {
+		this.fromString = fromString;
+	}
+	public String getWhereString() {
+		return whereString;
+	}
+	public void setWhereString(String whereString) {
+		this.whereString = whereString;
+	}
+	
 	public abstract SQLSelect select(String columnName);
 	public abstract SQLSelect from(String tablename);
 	public abstract SQLSelect where(String condition);
