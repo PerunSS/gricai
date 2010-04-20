f2e0d735b1831f7afc69fb9429a758d002608ede
churava13 <churava13@2bbab176-0796-11df-956f-01f849d29158>
2010-04-20 16:20:54 +0000

    Exception tests added

--- trunk/GricaI/src/com/gricai/central/server/dbManager/impl/SQLSelectTest.java
+++ trunk/GricaI/src/com/gricai/central/server/dbManager/impl/SQLSelectTest.java
@@ -25,20 +25,34 @@ public class SQLSelectTest {
 	}
 
 	@Test
-	public void test1()
-	{
-		
+	// tests SQLSelectImpl
+	public void test1()	{
 		select = new SQLSelectImpl();
 		try
 		{
 			assertEquals("SELECT nesto FROM negde", select.select("nesto").from("negde").evaluate());
 			assertEquals("SELECT nesto FROM negde,negde2 WHERE nesto", new SQLSelectImpl().select("nesto").from("negde").from("negde2").where("nesto").evaluate());
 			System.out.println("Test 1 completed successfully.");
-		}
-		catch (NullPointerException e)
-		{
+		} catch (NullPointerException e){
 			System.out.println(e.getMessage());
 		}
+	}
+	
+	@Test
+	//Tests SQLSelectImpl exceptions
+	public void test2(){
+		select =  new SQLSelectImpl();
+		try{
+			String s = select.evaluate();
+		} catch (NullPointerException e){
+			assertEquals("Null pointer exception:empty SELECT string", e.getMessage());
+		}
+		try{
+			String s = select.select("nesto").evaluate();
+		} catch ( NullPointerException e){
+			assertEquals("Null pointer exception:empty FROM string", e.getMessage());
+		}
+		System.out.println("Test 2 completed successfully.");
 	}
 
 	public static void main(String[] args) 
