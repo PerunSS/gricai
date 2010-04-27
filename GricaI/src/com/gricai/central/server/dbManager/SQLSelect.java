5a1d5ae5c9c3db5aae066af2e7aae885aab2cc0d
avaricak <avaricak@2bbab176-0796-11df-956f-01f849d29158>
2010-04-27 16:28:40 +0000

    added javadoc

--- GricaI/src/com/gricai/central/server/dbManager/SQLSelect.java
+++ GricaI/src/com/gricai/central/server/dbManager/SQLSelect.java
@@ -1,19 +1,56 @@
 package com.gricai.central.server.dbManager;
 
 import java.lang.reflect.Constructor;
+import java.sql.SQLException;
 
 import com.gricai.central.server.dbManager.exception.InvalidParameterException;
 import com.gricai.central.server.dbManager.impl.SQLSelectImpl;
 
 public abstract class SQLSelect extends SQLQuery{
 	
+	/**
+	 * 
+	 * @param columnName
+	 * @return
+	 */
 	public abstract SQLSelect select(String columnName);
-	public abstract SQLSelect from(String tablename) ;
-	public abstract SQLSelect where(String condition) throws Exception;
-	public abstract SQLSelect orderBy(String condition);
-	public abstract SQLSelect parameter(String paramName, Object paramValue) throws InvalidParameterException;
-	public abstract SQLSelect groupBy( String condition) ;
 	
+	/**
+	 * 
+	 * @param tablename
+	 * @return
+	 */
+	public abstract SQLSelect from(String tablename);
+	
+	/**
+	 * 
+	 * @param condition
+	 * @return
+ * @throws SQLException
+	 */
+	public abstract SQLSelect where(String condition) throws SQLException;
+	
+	/**
+	 * 
+	 * @param condition
+	 * @return
+	 */
+	public abstract SQLSelect orderBy(String condition);
+	
+	/**
+	 * 
+	 * @param paramName
+	 * @param paramValue
+	 * @return
+	 * @throws InvalidParameterException
+	 */
+	public abstract SQLSelect parameter(String paramName, Object paramValue) throws InvalidParameterException;
+	
+	/**
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
 	public static SQLSelect getInstance() throws Exception {
 		try{
 			Constructor<SQLSelectImpl> constructor = SQLSelectImpl.class.getDeclaredConstructor();
