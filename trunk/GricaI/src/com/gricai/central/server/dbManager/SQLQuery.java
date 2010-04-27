a2719a2a59ce382f622ca072491bf03949a49ddc
avaricak <avaricak@2bbab176-0796-11df-956f-01f849d29158>
2010-04-27 16:28:40 +0000

    added javadoc

--- trunk/GricaI/src/com/gricai/central/server/dbManager/SQLQuery.java
+++ trunk/GricaI/src/com/gricai/central/server/dbManager/SQLQuery.java
@@ -4,6 +4,18 @@ import java.sql.SQLException;
 
 public abstract class SQLQuery {
 
+	/**
+	 * evaluates any SQL Query to prepared statement string 
+	 * @return 
+	 * @throws Exception
+	 */
 	protected abstract String evaluate() throws Exception;
+	
+	/**
+	 * executes SQL Query and returns result 
+	 * @return 	ResultSet if SQLSelect
+	 * 			int if SQLDelete, SQLUpdate, SQLInsert
+	 * @throws SQLException
+	 */
 	public abstract Object execute() throws SQLException;
 }
