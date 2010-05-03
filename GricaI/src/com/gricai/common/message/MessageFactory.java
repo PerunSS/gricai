b7238c2338a71d3be27483ae7b30eb1c58d04d02
churava13 <churava13@2bbab176-0796-11df-956f-01f849d29158>
2010-05-03 15:04:30 +0000

    Message Factory implemented

--- GricaI/src/com/gricai/common/message/MessageFactory.java
+++ GricaI/src/com/gricai/common/message/MessageFactory.java
@@ -2,11 +2,44 @@ package com.gricai.common.message;
 
 import java.nio.ByteBuffer;
 
+import com.gricai.common.message.exception.WrongMessageTypeException;
+
 public class MessageFactory {
 
-	public static Message createMessage(ByteBuffer data) {
-		// TODO pogledati koji je tip poruke u pitanju, krairati objekat poruke,
-		// i pozvati fillMessage koji ce da napuni podatke u poruku
-		return null;
+	private static final String CHAT_MESSAGE = "ChatMessage";
+	private static final String ERROR_MESSAGE = "ErrorMessage";
+	private static final String JOIN_ROOM_MESSAGE = "JoinRoomMessage";
+	private static final String JOIN_ROOM_RESPONSE_MESSAGE = "JoinRoomResponseMessage";
+	private static final String LEAVE_ROOM_MESSAGE = "LeaveRoomMessage";
+	private static final String LOGIN_MESSAGE = "LoginMessage";
+	private static final String LOGIN_RESPONSE_MESSAGE = "LoginResponseMessage";
+	
+	public static Message createMessage(ByteBuffer data) throws WrongMessageTypeException{
+		byte[] bytes =  new byte[data.capacity()];
+		data.get(bytes, 0, bytes.length);
+		String fullMessage = new String(bytes);
+		String messageType = fullMessage.substring(fullMessage.indexOf('=') + 1,fullMessage.indexOf('&'));
+		Message message;
+		if ( messageType == CHAT_MESSAGE){
+			message = new ChatMessage();
+		} else if ( messageType == ERROR_MESSAGE){
+			message = new ErrorMessage();
+		} else if ( messageType == JOIN_ROOM_MESSAGE){
+			message = new JoinRoomMessage();
+		} else if ( messageType == JOIN_ROOM_RESPONSE_MESSAGE){
+			message = new JoinRoomResponseMessage();
+		} else if ( messageType == LEAVE_ROOM_MESSAGE){
+			message = new LeaveRoomMessage();
+		} else if ( messageType == LOGIN_MESSAGE){
+			message = new LoginMessage();
+		} else if ( messageType == LOGIN_RESPONSE_MESSAGE){
+			message = new LoginResponseMessage();
+		} else {
+			throw new WrongMessageTypeException("Wrong message type!");
+		}
+		
+		message.fillMessage(data);
+		
+		return message;
 	}
 }
