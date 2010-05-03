1ec41ec59cfd2108eae4f36dbdd698c6fed6d458
churava13 <churava13@2bbab176-0796-11df-956f-01f849d29158>
2010-05-03 12:30:40 +0000

    ChatMessage methods implemented ver1

--- GricaI/src/com/gricai/common/message/ChatMessage.java
+++ GricaI/src/com/gricai/common/message/ChatMessage.java
@@ -3,11 +3,34 @@ package com.gricai.common.message;
 import java.nio.ByteBuffer;
 
 public class ChatMessage implements Message {
+	
+	private String userName;
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
 
 	@Override
 	public void fillMessage(ByteBuffer data) {
-		// TODO Auto-generated method stub
-
+		byte[] bytes =  new byte[data.capacity()];
+		data.get(bytes, 0, bytes.length);
+		String fullMessage = new String(bytes);
+		setUserName(fullMessage.substring(fullMessage.indexOf('&'),fullMessage.indexOf('&',fullMessage.indexOf('&')) ));
+		setMessage(fullMessage.substring(fullMessage.indexOf('&', fullMessage.indexOf('&'))));
 	}
 
 	@Override
@@ -18,8 +41,8 @@ public class ChatMessage implements Message {
 
 	@Override
 	public ByteBuffer toByteBuffer() {
-		// TODO Auto-generated method stub
-		return null;
+		byte[] bytes = new String("Chat message &" + getUserName() + "&" + getMessage()).getBytes();
+		return ByteBuffer.wrap(bytes);
 	}
 
 }
