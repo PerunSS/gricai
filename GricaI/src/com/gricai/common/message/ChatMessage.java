5f57c02268475c1390e3ae72c482e4468b3f09ad
churava13 <churava13@2bbab176-0796-11df-956f-01f849d29158>
2010-05-03 13:14:59 +0000

    changes to ChatMessage

--- GricaI/src/com/gricai/common/message/ChatMessage.java
+++ GricaI/src/com/gricai/common/message/ChatMessage.java
@@ -12,31 +12,18 @@ public class ChatMessage implements Message {
 	private String username;
 	private String text;
 
-	public String getText() {
-		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
-	}
-
 
 	@Override
 	public void fillMessage(ByteBuffer data) {
 		byte[] bytes =  new byte[data.capacity()];
 		data.get(bytes, 0, bytes.length);
 		String fullMessage = new String(bytes);
-		Map<String, String> messageData = new HashMap<String, String>();
-		String parts[] = fullMessage.split("&");
-		for(String part:parts){
-			String key = part.substring(0,part.indexOf('='));
-			String value = part.substring(part.indexOf('=')+1);
-			messageData.put(key, value);
-		}
-		username = messageData.get(TEXT_USERNAME);
-		text = messageData.get(TEXT_TEXT);
-//		setUserName(fullMessage.substring(fullMessage.indexOf('&'),fullMessage.indexOf('&',fullMessage.indexOf('&')) ));
-//		setText(fullMessage.substring(fullMessage.indexOf('&', fullMessage.indexOf('&'))));
+		int indexOfUser = fullMessage.indexOf('&')+1;
+		int indexOfText = fullMessage.indexOf('&', indexOfUser);
+		String userString = fullMessage.substring(indexOfUser,indexOfText-1);
+		setUsername(userString.substring(userString.indexOf('=') + 1));
+		String textString = fullMessage.substring(indexOfText);
+		setText(textString.substring(textString.indexOf('=') + 1));
 	}
 
 	@Override
@@ -51,8 +38,14 @@ public class ChatMessage implements Message {
 	}
 
 
-	public void setUserName(String username) {
+	public void setUsername(String username) {
 		this.username = username;
 	}
+	public String getText() {
+		return text;
+	}
 
+	public void setText(String text) {
+		this.text = text;
+	}
 }
