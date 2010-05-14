f0402465af084c253937a39884b4b67bdd355fc4
churava13 <churava13@2bbab176-0796-11df-956f-01f849d29158>
2010-05-14 15:46:30 +0000

    MessageFactory.js implemented

--- trunk/GricaI/html/client/MessageFactory.js
+++ trunk/GricaI/html/client/MessageFactory.js
@@ -9,5 +9,37 @@ MessageFactory.prototype.createLoginMessage = function(values){
 }
 
 MessageFactory.prototype.createChatMessage = function(values){
-	
+	var names = ["username", "text"];
+	var msg = new Message( names, values, "ChatMessage");
+	return msg;
+}
+
+MessageFactory.prototype.createErrorMessage = function( values){
+	var names = ["errorCode"];
+	var msg = new Message( names, values, "ErrorMessage");
+	return msg;
+}
+
+MessageFactory.prototype.createJoinRoomMessage = function( values){
+	var names = ["username", "roomName"];
+	var msg = new Message( names, values, "JoinRoomMessage");
+	return msg;
+}
+
+MessageFactory.prototype.createJoinRoomResponseMessage = function( values){
+	var names = ["roomName", "canJoin"];
+	var msg = new Message( names, values, "JoinRoomResponseMessage");
+	return msg;
+}
+
+MessageFactory.prototype.createLeaveRoomMessage = function( values){
+	var names = [];
+	var msg = new Message( names, values, "LeaveRoomMessage");
+	return msg;
+}
+
+MessageFactory.prototype.createLoginResponseMessage = function( values){
+	var names = ["logged", "username"];
+	var msg = new Message( names, values, "LoginResponseMessage");
+	return msg;
 }
