ad8c799facb5f2da6aae41fd043a457c52578bbd
avaricak <avaricak@2bbab176-0796-11df-956f-01f849d29158>
2010-05-15 10:48:43 +0000

    added classes for sql insert and update

---
+++ GricaI/src/com/gricai/central/server/dbManager/impl/SQLInsertImpl.java
@@ -0,0 +1,21 @@
+package com.gricai.central.server.dbManager.impl;
+
+import java.sql.SQLException;
+
+import com.gricai.central.server.dbManager.SQLInsert;
+
+public class SQLInsertImpl extends SQLInsert {
+
+	@Override
+	protected String evaluate() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object execute() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
