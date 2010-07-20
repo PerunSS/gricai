ed626e6fe5382379f7505a3bf38b133400abd2fc
avaricak <avaricak@2bbab176-0796-11df-956f-01f849d29158>
2010-07-20 12:47:49 +0000

    added constructors

--- trunk/GricaI/src/com/gricai/central/client/gui/components/GricaIComboBox.java
+++ trunk/GricaI/src/com/gricai/central/client/gui/components/GricaIComboBox.java
@@ -2,6 +2,17 @@ package com.gricai.central.client.gui.components;
 
 import javax.swing.JComboBox;
 
+import com.gricai.central.client.gui.utils.GricaiComponentCreator;
+
 public class GricaIComboBox extends JComboBox {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public GricaIComboBox(String name, double x, double y, double width,
+			double height) {
+		GricaiComponentCreator.setParameters(this, name, x, y, width, height);
+	}
 }
--- trunk/GricaI/src/com/gricai/central/client/gui/components/GricaIList.java
+++ trunk/GricaI/src/com/gricai/central/client/gui/components/GricaIList.java
@@ -2,6 +2,17 @@ package com.gricai.central.client.gui.components;
 
 import javax.swing.JList;
 
+import com.gricai.central.client.gui.utils.GricaiComponentCreator;
+
 public class GricaIList extends JList {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public GricaIList(String name, double x, double y, double width,
+			double height) {
+		GricaiComponentCreator.setParameters(this, name, x, y, width, height);
+	}
 }
