6038001565b5c9ad0ac771a136347a2ef5b32017
churava13 <churava13@2bbab176-0796-11df-956f-01f849d29158>
2011-01-19 17:57:18 +0000

    RnB rumors n facts version 1 commited

---
+++ Android/trunk/RnB Rumors n Facts/src/com/cerSprikRu/rnbrumorsnfacts/RnBRumorsnFacts.java
@@ -0,0 +1,246 @@
+package com.cerSprikRu.rnbrumorsnfacts;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+
+import com.cerSprikRu.rnbrumorsnfacts.person.Person;
+
+public class RnBRumorsnFacts extends Activity {
+    /** Called when the activity is first created. */
+	
+	private GridView gridView;
+	private TextView factsNRumorsText;
+	private Button homeButton;
+	private Button nextButton;
+	private Button previousButton;
+	private Button randomButton;
+	private Person person;
+	
+	private static Map<Integer, Integer> personIds = new HashMap<Integer, Integer>();
+	static {
+		personIds.put(0, R.string.person0);
+		personIds.put(1, R.string.person1);
+		personIds.put(2, R.string.person2);
+		personIds.put(3, R.string.person3);
+		personIds.put(4, R.string.person4);
+		personIds.put(5, R.string.person5);
+		
+	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    
+        startMainView();
+        
+    }
+    
+    private void startMainView(){
+    	setContentView(R.layout.main);  
+    	
+    	gridView = (GridView) findViewById(R.id.startPageGridView);
+        gridView.setAdapter(new ImageAdapter(this));
+        gridView.setBackgroundColor(android.graphics.Color.WHITE);
+
+        gridView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+            	itemSelected( parent, v, position, id);
+            }
+
+			
+        });
+    }
+    
+    
+    private void itemSelected(AdapterView<?> parent, View v, int position, long id) {
+				setContentView(R.layout.selected);
+				
+				person = Person.readPerson(position, getString(personIds.get(position)));
+				
+				factsNRumorsText = (TextView) findViewById(R.id.rumorText);
+				factsNRumorsText.setText(person.getNext());
+				
+				
+				previousButton = (Button) findViewById(R.id.previousButton);
+				previousButton.setOnClickListener( new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						previousClick(v);
+					}
+				});
+				
+				nextButton = (Button) findViewById(R.id.nextButton);
+				nextButton.setOnClickListener( new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						nextClick(v);
+					}
+				});
+				
+				randomButton = (Button) findViewById(R.id.randomButton);
+				randomButton.setOnClickListener( new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						randomClick(v);
+					}
+				});
+				
+				
+				homeButton = (Button) findViewById(R.id.homeButton);
+		        homeButton.setOnClickListener( new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						homeClick(v);
+					}
+				});
+			}
+    
+    public void nextClick( View v){
+    	factsNRumorsText = (TextView) findViewById(R.id.rumorText);
+		factsNRumorsText.setText(person.getNext());
+		
+		
+		previousButton = (Button) findViewById(R.id.previousButton);
+		previousButton.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				previousClick(v);
+			}
+		});
+		
+		nextButton = (Button) findViewById(R.id.nextButton);
+		nextButton.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				nextClick(v);
+			}
+		});
+		
+		randomButton = (Button) findViewById(R.id.randomButton);
+		randomButton.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				randomClick(v);
+			}
+		});
+		
+		
+		homeButton = (Button) findViewById(R.id.homeButton);
+        homeButton.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				homeClick(v);
+			}
+		});
+    	
+    }
+    
+    public void randomClick( View v){
+    	factsNRumorsText = (TextView) findViewById(R.id.rumorText);
+		factsNRumorsText.setText(person.getRandom());
+		
+		
+		previousButton = (Button) findViewById(R.id.previousButton);
+		previousButton.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				previousClick(v);
+			}
+		});
+		
+		nextButton = (Button) findViewById(R.id.nextButton);
+		nextButton.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				nextClick(v);
+			}
+		});
+		
+		randomButton = (Button) findViewById(R.id.randomButton);
+		randomButton.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				randomClick(v);
+			}
+		});
+		
+		
+		homeButton = (Button) findViewById(R.id.homeButton);
+        homeButton.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				homeClick(v);
+			}
+		});
+    }
+    
+    public void previousClick( View v){
+    	factsNRumorsText = (TextView) findViewById(R.id.rumorText);
+    	factsNRumorsText.setText(person.getPrevious());
+		
+		
+		previousButton = (Button) findViewById(R.id.previousButton);
+		previousButton.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				previousClick(v);
+			}
+		});
+		
+		nextButton = (Button) findViewById(R.id.nextButton);
+		nextButton.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				nextClick(v);
+			}
+		});
+		
+		randomButton = (Button) findViewById(R.id.randomButton);
+		randomButton.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				randomClick(v);
+			}
+		});
+		
+		
+		homeButton = (Button) findViewById(R.id.homeButton);
+        homeButton.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				homeClick(v);
+			}
+		});
+		
+    }
+    
+    public void homeClick( View view){
+    	startMainView();
+    }
+    
+}
---
+++ Android/trunk/RnB Rumors n Facts/src/com/cerSprikRu/rnbrumorsnfacts/person/Person.java
@@ -0,0 +1,53 @@
+package com.cerSprikRu.rnbrumorsnfacts.person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+
+	private List<String> rumorsAndFacts;
+	private int currentIndex;
+	
+	private Person(){
+		
+	}
+	
+	public static Person readPerson( int personIndex, String rummors){
+		Person person = new Person();
+		person.currentIndex = -1;
+		String[] rmrArr = rummors.split(";");
+		person.rumorsAndFacts = new ArrayList<String>();
+		for(String str:rmrArr){
+			person.rumorsAndFacts.add(str);
+		}
+//		ctx.
+//		DBManager manager = new DBManager(ctx);
+//		Cursor rumors = manager.getAllTitles(personIndex);
+//		while(rumors.moveToNext()){
+//			person.rumorsAndFacts.add(rumors.getString(1));
+//		}
+		return person;
+	}
+	
+
+	public String getNext(){
+		currentIndex++;
+		if(currentIndex>=rumorsAndFacts.size()){
+			currentIndex = 0;
+		}
+		return rumorsAndFacts.get(currentIndex);
+	}
+	
+	public String getPrevious(){
+		currentIndex --;
+		if(currentIndex < 0)
+			currentIndex = rumorsAndFacts.size()-1;
+		return rumorsAndFacts.get(currentIndex);
+	}
+	
+	public String getRandom(){
+		currentIndex = (int)(Math.random() *rumorsAndFacts.size());
+		return rumorsAndFacts.get(currentIndex);
+	}
+}
+
