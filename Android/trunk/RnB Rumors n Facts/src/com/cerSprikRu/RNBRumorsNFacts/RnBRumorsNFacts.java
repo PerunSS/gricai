eca147fb4a290456a7a8c5a81dd9cd757fc3b9de
avaricak <avaricak@2bbab176-0796-11df-956f-01f849d29158>
2011-01-20 14:56:22 +0000

    added list of persons

--- Android/trunk/RnB Rumors n Facts/src/com/cerSprikRu/RNBRumorsNFacts/RnBRumorsNFacts.java
+++ Android/trunk/RnB Rumors n Facts/src/com/cerSprikRu/RNBRumorsNFacts/RnBRumorsNFacts.java
@@ -1,13 +1,22 @@
 package com.cerSprikRu.RNBRumorsNFacts;
 
+import java.util.List;
+
+import com.cerSprikRu.RNBRumorsNFacts.db.customManager.DBManager;
+import com.cerSprikRu.RNBRumorsNFacts.person.Person;
+
 import android.app.Activity;
 import android.os.Bundle;
 
 public class RnBRumorsNFacts extends Activity {
+	
+	List<Person> persons;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DBManager manager = new DBManager(this);
+        persons = manager.read();
         startApplication();
        
     }
