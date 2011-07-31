d7ea88cf3471ccbf1b89b963053e8a5aeb6e3a30
baskess@gmail.com <baskess@gmail.com@2bbab176-0796-11df-956f-01f849d29158>
2011-07-31 11:45:47 +0000

    Chris Brown Facts update

--- Android/trunk/Chris Brown Facts/src/com/cerSprikRu/grind/CBrownFacts/CBrownFacts.java
+++ Android/trunk/Chris Brown Facts/src/com/cerSprikRu/grind/CBrownFacts/CBrownFacts.java
@@ -34,7 +34,9 @@ public class CBrownFacts extends Activity {
 			R.drawable.b20, R.drawable.b21, R.drawable.b22, R.drawable.b23,
 			R.drawable.b24, R.drawable.b25, R.drawable.b26, R.drawable.b27,
 			R.drawable.b28, R.drawable.b29, R.drawable.b30, R.drawable.b31,
-			R.drawable.b32, R.drawable.b33, R.drawable.b34, R.drawable.b35,};
+			R.drawable.b32, R.drawable.b33, R.drawable.b34, R.drawable.b35,
+			R.drawable.b36, R.drawable.b37, R.drawable.b38, R.drawable.b39,
+			R.drawable.b40,};
 
 	private int currentBck;
 	private AlertDialog.Builder builder;
@@ -131,7 +133,7 @@ public class CBrownFacts extends Activity {
 
 				intent.setType("text/plain");
 				intent.putExtra(Intent.EXTRA_TEXT,
-						"Chris Brown: "
+						"Fact about Chris Brown: "
 								+fact);
 
 				startActivity(Intent.createChooser(intent, "share"));
