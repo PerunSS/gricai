3cf23c802aa88a04575ff87afa53675fa82aa009
baskess@gmail.com <baskess@gmail.com@2bbab176-0796-11df-956f-01f849d29158>
2011-08-25 11:52:20 +0000

    eminem facts up

--- Android/trunk/EminemFacts/src/com/cerSprikRu/EminemFacts/EminemFacts.java
+++ Android/trunk/EminemFacts/src/com/cerSprikRu/EminemFacts/EminemFacts.java
@@ -34,7 +34,9 @@ public class EminemFacts extends Activity {
 			R.drawable.b20, R.drawable.b21, R.drawable.b22, R.drawable.b23,
 			R.drawable.b24, R.drawable.b25, R.drawable.b26, R.drawable.b27,
 			R.drawable.b28, R.drawable.b29, R.drawable.b30, R.drawable.b31,
-			R.drawable.b32, R.drawable.b33, R.drawable.b34, R.drawable.b35 };
+			R.drawable.b32, R.drawable.b33, R.drawable.b34, R.drawable.b35,
+			R.drawable.b36, R.drawable.b37, R.drawable.b38, R.drawable.b39,
+			R.drawable.b40, R.drawable.b41,};
 
 	private int currentBck;
 	private AlertDialog.Builder builder;
