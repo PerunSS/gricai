f74660d87fc187062e5f942bca54355ddbcfb245
churava13@gmail.com <churava13@gmail.com@2bbab176-0796-11df-956f-01f849d29158>
2011-09-27 20:46:18 +0000

    forme neke

---
+++ trunk/prs_ecommerce/dev/root/register_fl.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+</head>
+<body>
+	<form
+		action="TODO register fizickog lica action"
+		method="post"
+		target="TODO target"
+	>
+		<fieldset>
+			<legend>Register</legend>
+			Ime: <input
+				type="text"
+				name="ime"
+				id="ime"
+			><br />Prezime: <input
+				type="text"
+				name="prezime"
+				id="prezime"
+			><br />JMBG: <input
+				type="text"
+				name="jmbg"
+				id="jmbg"
+			><br />Kontakt telefon: <input
+				type="text"
+				name="telefon"
+				id="telefon"
+			><br />Email: <input
+				type="text"
+				name="email"
+				id="email"
+			><br /> <input
+				type="submit"
+				name="sub"
+				id="sub"
+				value="Register"
+			>
+		</fieldset>
+	</form>
+	
+</body>
+</html>
