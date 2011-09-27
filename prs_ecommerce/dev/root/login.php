e5c49e3aefd4b2bf6cc711d16f715324e9afc810
churava13 <churava13@gmail.com>
2011-09-27 20:46:18 +0000

    forme neke

---
+++ prs_ecommerce/dev/root/login.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+</head>
+<body>
+	<form
+		action="TODO register fizickog lica action"
+		method="post"
+		target="TODO target"
+	>
+		<fieldset>
+			<legend>Login</legend>
+			Korisnicko ime: <input
+				type="text"
+				name="korisnicko_ime"
+				id="korisnicko_ime"
+			><br />Sifra: <input
+				type="text"
+				name="sifra"
+				id="sifra"
+			><br /> <input
+				type="submit"
+				name="sub"
+				id="sub"
+				value="Login"
+			>
+		</fieldset>
+	</form>
+</body>
+</html>
---
+++ prs_ecommerce/dev/root/test.php
@@ -0,0 +1,119 @@
+<html>
+<head>
+</head>
+<body>
+	<iframe
+		width="800px"
+		height="600px"
+		id="myFrame"
+		name="myFrame"
+	></iframe>
+	<form
+		action="TODO register action"
+		method="post"
+		target="myFrame"
+	>
+		<fieldset>
+			<legend>Register</legend>
+			Ime: <input
+				type="text"
+				name="ime"
+				id="ime"
+			><br />Prezime: <input
+				type="text"
+				name="prezime"
+				id="prezime"
+			><br />JMBG: <input
+				type="text"
+				name="jmbg"
+				id="jmbg"
+			><br />Kontakt telefon: <input
+				type="text"
+				name="telefon"
+				id="telefon"
+			><br />Email: <input
+				type="text"
+				name="email"
+				id="email"
+			><br /> <input
+				type="submit"
+				name="sub"
+				id="sub"
+				value="Register"
+			>
+		</fieldset>
+	</form>
+
+	<form
+		action="TODO register pravnog lica action"
+		method="post"
+		target="myFrame"
+	>
+		<fieldset>
+			<legend>Register</legend>
+			Ime firme: <input
+				type="text"
+				name="ime"
+				id="ime"
+			><br />Adresa: <input
+				type="text"
+				name="adresa"
+				id="adresa"
+			><br />Grad: <input
+				type="text"
+				name="grad"
+				id="grad"
+			><br />PIB: <input
+				type="text"
+				name="pib"
+				id="pib"
+			><br />Maticni broj: <input
+				type="text"
+				name="maticni_broj"
+				id="maticni_broj"
+			><br />Kontakt telefon: <input
+				type="text"
+				name="telefon"
+				id="telefon"
+			><br />Email: <input
+				type="text"
+				name="email"
+				id="email"
+			><br />Adresa za dostavu: <input
+				type="text"
+				name="adresa_za_dostavu"
+				id="adresa_za_dostavu"
+			><br /> <input
+				type="submit"
+				name="sub"
+				id="sub"
+				value="Register"
+			>
+		</fieldset>
+	</form>
+	<form
+		action="TODO register fizickog lica action"
+		method="post"
+		target="myFrame"
+	>
+		<fieldset>
+			<legend>Login</legend>
+			Korisnicko ime: <input
+				type="text"
+				name="korisnicko_ime"
+				id="korisnicko_ime"
+			><br />Sifra: <input
+				type="text"
+				name="sifra"
+				id="sifra"
+			><br /> <input
+				type="submit"
+				name="sub"
+				id="sub"
+				value="Login"
+			>
+		</fieldset>
+	</form>
+	
+</body>
+</html>
