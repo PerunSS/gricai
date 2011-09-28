5700e23ea1d6004c815dfa6832f4818804c0b562
nisam.lep.ali.sam.ruzan@gmail.com <nisam.lep.ali.sam.ruzan@gmail.com@2bbab176-0796-11df-956f-01f849d29158>
2011-09-28 16:52:36 +0000

    login + exceptions

--- trunk/prs_ecommerce/dev/actions/user/user_login.php
+++ trunk/prs_ecommerce/dev/actions/user/user_login.php
@@ -1,4 +1,18 @@
-<?php 
-$user = new Users();
-print_r($user->login());
+<?php
+try{
+	$user=new Users();
+	$login=$user->login();
+	//$user_string = json_decode($login);	
+	//$_SESSION['user'] = json_encode($user_string->user);
+	print_r($login);
+}
+catch (SecurityExceptions $se){
+	print_r($se->getJSON());
+}
+catch (UsersExceptions $ue){
+	print_r($ue->getJSON());
+}
+catch (Exception $e){
+	//TODO log
+}
 ?>
