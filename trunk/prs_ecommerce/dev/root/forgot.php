a836d2786bc297f8f7bc9a90576b75a905f20345
churava13@gmail.com <churava13@gmail.com@2bbab176-0796-11df-956f-01f849d29158>
2011-09-28 17:00:53 +0000

    jos malo formi

---
+++ trunk/prs_ecommerce/dev/root/forgot.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+</head>
+<body>
+	<form
+		action="TODO forgot password action"
+		method="post"
+		target="TODO target"
+	>
+		<fieldset>
+			<legend>Recover forgotten password</legend>
+			Email: <input
+				type="text"
+				name="email"
+				id="email"
+			><br /> <input
+				type="submit"
+				name="sub"
+				id="sub"
+				value="Forgot"
+			>
+		</fieldset>
+	</form>
+	
+</body>
+</html>
---
+++ trunk/prs_ecommerce/dev/root/update_fl.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+</head>
+<body>
+	<form
+		action="TODO update za fizicka lica action"
+		method="post"
+		target="TODO target"
+	>
+		<fieldset>
+			<legend>Promenite kontakt telefon</legend>
+			Novi telefon<input
+				type="text"
+				name="telefon"
+				id="telefon"
+			><br /> <input
+				type="submit"
+				name="sub"
+				id="sub"
+				value="update_fl"
+			>
+		</fieldset>
+	</form>
+	
+</body>
+</html>
