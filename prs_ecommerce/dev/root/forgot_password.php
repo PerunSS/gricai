6050b5f39ce9a263fd8ed0d1ca9422b131b356be
churava13 <churava13@2bbab176-0796-11df-956f-01f849d29158>
2011-10-04 18:14:28 +0000

    forgot password

---
+++ prs_ecommerce/dev/root/forgot_password.php
@@ -0,0 +1,4 @@
+<form action='/action.php' method = 'post'>
+	Unesite email : <input type="text" name="email" id="email"/><br />
+	<input type="submit" name="forgot_password" value="promeni password" /> 
+</form>
