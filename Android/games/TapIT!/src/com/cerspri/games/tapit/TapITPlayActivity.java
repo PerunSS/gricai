8b922fee7ccb2f7ca4997b1049bc54b83b6fa2e4
avaricak <avaricak@2bbab176-0796-11df-956f-01f849d29158>
2012-02-21 21:49:39 +0000

    doradjena pauza

--- Android/games/TapIT!/src/com/cerspri/games/tapit/TapITPlayActivity.java
+++ Android/games/TapIT!/src/com/cerspri/games/tapit/TapITPlayActivity.java
@@ -37,14 +37,25 @@ public class TapITPlayActivity extends Activity {
 	@Override
 	public void onBackPressed() {
 		panel.pause();
-		final Dialog dialog = new Dialog(this, android.R.style.Theme_Black);
+		final Dialog dialog = new Dialog(this, android.R.style.Theme_Black){
+			 @Override
+	            public boolean onKeyDown(int keyCode, KeyEvent event) {
+	                if(keyCode == KeyEvent.KEYCODE_BACK) {
+	                	dismiss();
+	    				panel.continiue();
+	                    return true;
+	                }
+
+	                return super.onKeyDown(keyCode, event);
+	            }
+		};
 		dialog.setContentView(R.layout.pause_dialog);
 		dialog.setTitle("PAUSE");
 		TextView gameScoreVeiw = (TextView) dialog
 				.findViewById(R.id.game_score);
 		gameScoreVeiw.setText(panel.getScore() + ".0");
 		TextView maxTimeVeiw = (TextView) dialog.findViewById(R.id.max_time);
-		maxTimeVeiw.setText(panel.getMaxTime() + "");
+		maxTimeVeiw.setText(panel.getMaxTime()/1000.0 + "");
 		Button dismissButton = (Button) dialog.findViewById(R.id.back);
 		dismissButton.setOnClickListener(new View.OnClickListener() {
 
