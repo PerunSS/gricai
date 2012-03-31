a28401bf159b1619ae8fd43ecf9b1f78b3982f38
avaricak <avaricak@2bbab176-0796-11df-956f-01f849d29158>
2012-03-31 14:06:25 +0000

    added refresh and session

--- dndb/frontend/dev/root/game.php
+++ dndb/frontend/dev/root/game.php
@@ -1,2 +1,14 @@
+<html>
+<head>
+<meta http-equiv="refresh" content="10;url=game.php">
+<title>game</title>
+</head>
+
 <?php
-echo "igra pocinje";
+require_once "../classes/AutoloadClasses.inc";
+if (!isset($_SESSION)) {
+	session_start();
+}
+?>
+
+</html>
